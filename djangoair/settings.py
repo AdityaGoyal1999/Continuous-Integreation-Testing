222ac160c88710fba4ef454c9e33ea25fe51017f
Aditya Goyal <adityagoyaldoon@gmail.com>
2020-06-28 16:10:57 +0530

    DB settings

--- djangoair/settings.py
+++ djangoair/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'postgres',
+        'HOST': 'db',
         'POST': 5432, 
     }
 }
